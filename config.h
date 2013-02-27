23ce0914362985ff9c4f149b247731591b1c3ed4
Michishige Kaito <me@mkaito.com>
2013-02-27 04:32:56 +0000

    Replaced tag names with icons. Added layout icons.

--- config.h
+++ config.h
@@ -4,26 +4,47 @@
 static const char font[] = "-*-xbmicons-medium-r-*-*-12-*-*-*-*-*-*-*" ","
                            "-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*";
 #define NUMCOLORS 18
+
+// Solarized													 
+#define S_yellow "#b58900"
+#define S_orange "#cb4b16"
+#define S_red "#dc322f"
+#define S_magenta "#d33682"
+#define S_violet "#6c71c4"
+#define S_blue "#268bd2"
+#define S_cyan "#2aa198"
+#define S_green "#859900"
+
+// Dark
+#define S_base03 "#002b36"
+#define S_base02 "#073642"
+#define S_base01 "#586e75"
+#define S_base00 "#657b83"
+#define S_base0 "#839496"
+#define S_base1 "#93a1a1"
+#define S_base2 "#eee8d5"
+#define S_base3 "#fdf6e3"
+
 static const char colors[NUMCOLORS][ColLast][9] = {
-  // border foreground background
-  { "#282a2e", "#373b41", "#1d1f21" }, // 1 = normal (grey on black)
-  { "#f0c674", "#c5c8c6", "#1d1f21" }, // 2 = selected (white on black)
-  { "#dc322f", "#1d1f21", "#f0c674" }, // 3 = urgent (black on yellow)
-  { "#282a2e", "#282a2e", "#1d1f21" }, // 4 = darkgrey on black
-  { "#282a2e", "#cc6666", "#282a2e" }, // 5 = red on darkgrey
-  { "#282a2e", "#b5bd68", "#282a2e" }, // 6 = green on darkgrey
-  { "#282a2e", "#f0c674", "#282a2e" }, // 7 = yellow on darkgrey
-  { "#282a2e", "#81a2be", "#282a2e" }, // 8 = blue on darkgrey
-  { "#282a2e", "#b294bb", "#282a2e" }, // 9 = magenta on darkgrey
-  { "#282a2e", "#8abeb7", "#282a2e" }, // A = cyan on darkgrey
-  { "#282a2e", "#A54242", "#282a2e" }, // B = darkred on darkgrey
-  { "#282a2e", "#8C9440", "#282a2e" }, // C = darkgreen on darkgrey
-  { "#282a2e", "#DE935F", "#282a2e" }, // D = darkyellow on darkgrey
-  { "#282a2e", "#5F819D", "#282a2e" }, // E = darkblue on darkgrey
-  { "#282a2e", "#85678F", "#282a2e" }, // F = darkmagenta on darkgrey
-  { "#282a2e", "#5E8D87", "#282a2e" }, //10 = darkcyan on darkgrey
-  { "#282a2e", "#969896", "#282a2e" }, //11 = lightgrey on darkgrey
-  { "#282a2e", "#C5C8C6", "#282a2e" }  //12 = white on darkgrey
+  //	border		foreground background
+  {		S_base01,	S_base01 , S_base03	 }, // 1 = normal (grey on black)
+  {		S_yellow,	S_base1  , S_base03	 }, // 2 = selected (white on black)
+  {		S_orange,	S_orange , S_base03	 }, // 3 = urgent (black on yellow)
+  {		S_violet,	S_base03 , S_base03	 }, // 4 = darkgrey on black
+  {		S_violet,	S_red    , S_base03	 }, // 5 = red on darkgrey
+  {		S_violet,	S_green  , S_base03	 }, // 6 = green on darkgrey
+  {		S_violet,	S_yellow , S_base03	 }, // 7 = yellow on darkgrey
+  {		S_violet,	S_blue   , S_base03	 }, // 8 = blue on darkgrey
+  {		S_violet,	S_magenta, S_base03	 }, // 9 = magenta on darkgrey
+  {		S_violet,	S_cyan   , S_base03	 }, // A = cyan on darkgrey
+  {		S_violet,	S_red    , S_base03	 }, // B = darkred on darkgrey
+  {		S_violet,	S_green  , S_base03	 }, // C = darkgreen on darkgrey
+  {		S_violet,	S_yellow , S_base03	 }, // D = darkyellow on darkgrey
+  {		S_violet,	S_blue   , S_base03	 }, // E = darkblue on darkgrey
+  {		S_violet,	S_magenta, S_base03	 }, // F = darkmagenta on darkgrey
+  {		S_violet,	S_cyan   , S_base03	 }, //10 = darkcyan on darkgrey
+  {		S_violet,	S_base01 , S_base03	 }, //11 = lightgrey on darkgrey
+  {		S_violet,	S_base1  , S_base03	 }  //12 = white on darkgrey
 };
 
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -33,47 +54,47 @@ static const Bool topbar            = True;     /* False means bottom bar */
 
 static const Layout layouts[] = {
 					/* symbol     arrange function */
-	/* 0 */	{ "[]=",      tile },    /* first entry is default */
-	/* 1 */	{ "><>",      NULL },    /* no layout function means floating behavior */
-	/* 2 */	{ "[M]",      monocle },
-	/* 3 */	{ "[G]",      gaplessgrid },
-	/* 4 */	{ "[B]",      bstack },
+	/* 0 */	{ "\uE019 \uE009 \uE019",      tile },    /* first entry is default */
+	/* 1 */	{ "\uE019 \uE00A \uE019",      NULL },    /* no layout function means floating behavior */
+	/* 2 */	{ "\uE019 \uE00B \uE019",      monocle },
+	/* 3 */	{ "\uE019 \uE00D \uE019",      gaplessgrid },
+	/* 4 */	{ "\uE019 \uE00D \uE019",      bstack },
 };  	
 
 /* tagging */
 //static const char *tags[] = { "term", "code", "web", "IM", "Gaming", "aux", "aux", "Media", "float" };
 static const Tag tags[] = {
 	//name			layout					mfact			nmaster
-	{ "term",		&layouts[0],		-1,				-1 },
-	{ "code",		&layouts[0],		 0.33,		-1 },
-	{ "web",		&layouts[2],		-1,				-1 },
-	{ "IM",			&layouts[0],		0.60,			-1 },
-	{ "gaming", &layouts[2],		-1,				-1 },
-	{ "aux",		&layouts[2],		-1,				-1 },
-	{ "media",	&layouts[2],		-1,				-1 },
-	{ "float",	&layouts[1],		-1,				-1 },
+	{ "\uE002",	&layouts[0],		-1,				-1 },
+	{ "\uE003",	&layouts[0],		 0.33,		-1 },
+	{ "\uE000",	&layouts[2],		-1,				-1 },
+	{ "\uE013",	&layouts[0],		0.60,			-1 },
+	{ "\uE006",	&layouts[2],		-1,				-1 },
+	{ "\uE007",	&layouts[2],		-1,				-1 },
+	{ "\uE008",	&layouts[2],		-1,				-1 },
+	{ "\uE001",	&layouts[1],		-1,				-1 },
 };
 
 static const Rule rules[] = {
-	//class						instance			title				tags mask		isfloating	iscentred		monitor
-	{ "Gimp",					NULL,					NULL,				0,					True,				False,			-1 },
-	{ NULL,						"ranger", 		NULL,				1 << 6,			False,			False,			-1 },
-	{ "MPlayer",			NULL,					NULL,				1 << 6,			False,			True,				-1 },
-	{ "mplayer2",			NULL,					NULL,				1 << 6,			False,			True,				-1 },
-	{ "Vlc",					NULL,					NULL,				1 << 6,			True,				True,				-1 },
-	{ "XFontSel",			NULL,					NULL,				0,					True,				True,				-1 },
-	{ "Chromium",			NULL,					NULL,				1 << 2,			False,			False,			-1 },
-	{ "Firefox",			NULL,					NULL,				1 << 2,			True,				True,				-1 },
-	{ "Firefox",			"Navigator",	NULL,				1 << 2,			False,			False,			-1 },
-	{ "Emacs",	  		"emacs",			NULL,				1 << 1,			False,			False,			-1 },
-	{ "Emacs",				"_Remember_",	NULL,				0,					True,				True,				-1 },
-	{ "Anki",					NULL,					NULL,				0,					True,				True,				-1 },
-	{ NULL,						"weechat",		NULL,				1 << 3,			False,			False,			-1 },
-	{ NULL,						"mail",				NULL,				1 << 3,			False,			False,			-1 },
-	{ NULL,						"mixer",			NULL,				0,					True,				True,				-1 },
-	{ NULL,						"music",			NULL,				0,					True,				True,				-1 },
-	{ "Galculator",		NULL,					NULL,				0,					True,				True,				-1 },
-	{ "Xmessage",			"xmessage",		NULL,				0,					True,				True,				-1 },
+	//class        , instance     , title , tags mask , isfloating , iscentred , monitor
+	{ "Gimp"       , NULL         , NULL  , 0         , True       , False     , -1 }    ,
+	{ NULL         , "ranger"     , NULL  , 1 << 6    , False      , False     , -1 }    ,
+	{ "MPlayer"    , NULL         , NULL  , 1 << 6    , False      , True      , -1 }    ,
+	{ "mplayer2"   , NULL         , NULL  , 1 << 6    , False      , True      , -1 }    ,
+	{ "Vlc"        , NULL         , NULL  , 1 << 6    , True       , True      , -1 }    ,
+	{ "XFontSel"   , NULL         , NULL  , 0         , True       , True      , -1 }    ,
+	{ "Chromium"   , NULL         , NULL  , 1 << 2    , False      , False     , -1 }    ,
+	{ "Firefox"    , NULL         , NULL  , 1 << 2    , True       , True      , -1 }    ,
+	{ "Firefox"    , "Navigator"  , NULL  , 1 << 2    , False      , False     , -1 }    ,
+	{ "Emacs"      , "emacs"      , NULL  , 1 << 1    , False      , False     , -1 }    ,
+	{ "Emacs"      , "_Remember_" , NULL  , 0         , True       , True      , -1 }    ,
+	{ "Anki"       , NULL         , NULL  , 0         , True       , True      , -1 }    ,
+	{ NULL         , "weechat"    , NULL  , 1 << 3    , False      , False     , -1 }    ,
+	{ NULL         , "mail"       , NULL  , 1 << 3    , False      , False     , -1 }    ,
+	{ NULL         , "mixer"      , NULL  , 0         , True       , True      , -1 }    ,
+	{ NULL         , "music"      , NULL  , 0         , True       , True      , -1 }    ,
+	{ "Galculator" , NULL         , NULL  , 0         , True       , True      , -1 }    ,
+	{ "Xmessage"   , "xmessage"   , NULL  , 0         , True       , True      , -1 }    ,
 };
 
 /* layout(s) */
@@ -91,25 +112,26 @@ static const Bool resizehints = False; /* True means respect size hints in tiled
 
 // Commands
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-static const char terminal[]				= "urxvt";
+static const char terminal[] = "termite";
 
-static const char *dmenucmd[]				= { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
-static const char *termcmd[]				= { terminal, NULL };
-static const char *musiccmd[]				= { terminal, "-name", "music", "-e", "ncmpcpp", NULL };
-static const char *filemancmd[]			= { terminal, "-name", "ranger", "-e", "ranger", NULL };
-static const char *browsercmd[]			= { "/bin/sh", "-c", "GTK2_RC_FILES=/usr/share/themes/Aurora/gtk-2.0/gtkrc firefox", NULL };
-static const char *imcmd[]					= { terminal, "-name", "weechat", "-e", "screen", "-l", "-UDRS", "weechat", "weechat-curses", NULL };
-static const char *emacscmd[]				= { "emacsclient", "-c", "-n", NULL };
-static const char *remembercmd[]		= { "emacsclient", "--eval", "(make-remember-frame)", NULL };
-static const char *mailcmd[]				= { terminal, "-name", "mail", "-e", "mutt", NULL };
-static const char *volmcmd[]				= { "amixer", "-q", "sset", "Master", "toggle", NULL };
-static const char *voldcmd[]				= { "amixer", "-q", "sset", "Master", "5-", "unmute", NULL };
-static const char *volucmd[]				= { "amixer", "-q", "sset", "Master", "5+", "unmute", NULL };
-static const char *mpdtogglecmd[]		= { "ncmpcpp", "toggle", NULL };
-static const char *mpdstopcmd[]			= { "ncmpcpp", "stop", NULL };
-static const char *mpdprevcmd[]			= { "ncmpcpp", "prev", NULL };
-static const char *mpdnextcmd[]			= { "ncmpcpp", "next", NULL };
-static const char *screenshotcmd[]	= { "scrot", "-q", "100", "-e", "mv $f ~/screenshots/ 2>/dev/null", NULL };
+static const char *dmenucmd[]      = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
+static const char *termcmd[]       = { terminal, NULL };
+static const char *musiccmd[]      = { terminal, "--name=music", "-e", "ncmpcpp", NULL };
+static const char *filemancmd[]    = { terminal, "--name=ranger", "-e", "ranger", NULL };
+static const char *browsercmd[]    = { "/bin/sh", "-c", "GTK2_RC_FILES = /usr/share/themes/Aurora/gtk-2.0/gtkrc firefox", NULL };
+static const char *imcmd[]         = { terminal, "--name=weechat", "-e", "weechat-curses", NULL };
+static const char *emacscmd[]      = { "emacsclient", "-c", "-n", NULL };
+static const char *remembercmd[]   = { "emacsclient", "--eval", "(make-remember-frame)", NULL };
+static const char *mailcmd[]       = { terminal, "--name=mail", "-e", "mutt", NULL };
+static const char *mixercmd[]      = { terminal, "--name=mixer", "-e", "alsamixer", NULL };
+static const char *volmcmd[]       = { "amixer", "-q", "sset", "Master", "toggle", NULL };
+static const char *voldcmd[]       = { "amixer", "-q", "sset", "Master", "5-", "unmute", NULL };
+static const char *volucmd[]       = { "amixer", "-q", "sset", "Master", "5+", "unmute", NULL };
+static const char *mpdtogglecmd[]  = { "ncmpcpp", "toggle", NULL };
+static const char *mpdstopcmd[]    = { "ncmpcpp", "stop", NULL };
+static const char *mpdprevcmd[]    = { "ncmpcpp", "prev", NULL };
+static const char *mpdnextcmd[]    = { "ncmpcpp", "next", NULL };
+static const char *screenshotcmd[] = { "scrot", "-q", "100", "-e", "mv $f ~/screenshots/ 2>/dev/null", NULL };
 
 static Key keys[] = {
 	/* modifier                     key						function        argument */
@@ -131,6 +153,7 @@ static Key keys[] = {
 
 	// Media keys
 	{ 0,														0x1008ff12,		spawn,					{.v = volmcmd } },
+	{ MODKEY,												0x1008ff12,		spawn,					{.v = mixercmd } },
 	{ 0,														0x1008ff11,		spawn,					{.v = voldcmd } },
 	{ 0,														0x1008ff13,		spawn,					{.v = volucmd } },
 	{ 0,														0x1008ff14,		spawn,					{.v = mpdtogglecmd } },
@@ -140,7 +163,7 @@ static Key keys[] = {
 	{ 0,														XK_Print,			spawn,					{.v = screenshotcmd } },
 
 	// WM Stuff
-	{ MODKEY,                       XK_b,					togglebar,			{0} },
+	{ MODKEY,                       XK_v,					togglebar,			{0} },
 	{ MODKEY,                       XK_j,					focusstack,			{.i = +1 } },
 	{ MODKEY,                       XK_k,					focusstack,			{.i = -1 } },
 
@@ -159,6 +182,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,					setlayout,			{.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,					setlayout,			{.v = &layouts[2]} },
 	{ MODKEY,                       XK_g,					setlayout,			{.v = &layouts[3]} },
+	{ MODKEY,                       XK_b,					setlayout,			{.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,			setlayout,			{0} },
 	{ MODKEY|ShiftMask,             XK_space,			togglefloating,	{0} },
 	{ MODKEY,                       XK_0,					view,						{.ui = ~0 } },
